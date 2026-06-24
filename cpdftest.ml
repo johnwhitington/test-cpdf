0e253ecb50e7a88eae92df429a785b9e3a5f681b
John Whitington <john@coherentgraphics.co.uk>
2026-06-24 12:17:55 +0100

    More redaction tests

--- cpdftest.ml
+++ cpdftest.ml
@@ -382,7 +382,8 @@ let tests =
    "-reveal-text-blacktext", ("", "-reveal-text AND -blacktext");
    "-test-extract-text", ("", "-test-extract-text");
    "-remove-stream-content", ("", "-remove-stream-content -no-preserve-objstm");
-   "-redact-shape", ("", "-redact-shape '100 100 200 300' -outline -recrypt -decrypt-force");
+   "-redact-shape", ("", "-redact-shape '100 100 200 300' -progress -outline -recrypt -decrypt-force");
+   "-redact-shape-info", ("", "-redact-shape '100 100 200 300' -process-images-info -outline -recrypt -decrypt-force");
    "-redact-whole-page", ("", "-redact-shape 'CMINX CMINY CW CH' -outline -recrypt -decrypt-force");
    "-redact-apply", ("", "-redact-apply -recrypt -decrypt-force");
    (* Specials, for internal testing *)
